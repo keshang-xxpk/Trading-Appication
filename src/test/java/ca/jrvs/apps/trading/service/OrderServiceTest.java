61bff7117b10f101f5fd7cf4a97f9cd0386d8095
keshang <ke.shang.jrvs.ca@gmail.com>
2019-07-31 13:59:45 +0000

    finish the rest part

---
+++ src/test/java/ca/jrvs/apps/trading/service/OrderServiceTest.java
@@ -0,0 +1,20 @@
+package ca.jrvs.apps.trading.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+    @Test
+    public void executeMarketOrder() {
+    }
+
+    @Test
+    public void handleBuyMarketOrder() {
+    }
+
+    @Test
+    public void handleSellMarketOrder() {
+    }
+}
